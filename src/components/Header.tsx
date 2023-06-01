f24287a6e35a002372379316a92132a098c406ff
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-06-01 19:26:43 +0300

    разбивка на компоненты

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Header = () => {
+    return  <header className={'header'}>
+        <img src={'https://w7.pngwing.com/pngs/526/232/png-transparent-earth-airplane-earth-3d-computer-graphics-globe-computer-wallpaper.png'}/>
+    </header >
+    
+};
+
