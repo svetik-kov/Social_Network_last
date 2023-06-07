704b08aa27c7dac9d11fda1736a374458a05d9f6
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-06-07 20:09:21 +0300

    14.css-модули, css-modules

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import s from './Header.module.css'
 
 export const Header = () => {
-    return  <header className={'header'}>
+    return  <header className={s.header}>
         <img src={'https://w7.pngwing.com/pngs/526/232/png-transparent-earth-airplane-earth-3d-computer-graphics-globe-computer-wallpaper.png'}/>
     </header >
     
