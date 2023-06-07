a2b57e9c976038340ab62382c999661116787491
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-06-07 22:05:31 +0300

    18. страница Dialogs

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,7 +4,7 @@ import {MyPosts} from './Posts/MyPosts';
 
 export const Profile = () => {
     return (
-        <div className={s.content}>
+        <div >
             <div>
                 <img
                     src="https://avatars.mds.yandex.net/i?id=73b1ef3734c95c8255c0f3669492b58d280f4dc9-8410613-images-thumbs&n=13"/>
