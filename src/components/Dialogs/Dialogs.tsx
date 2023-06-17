dcadb1b8d1087120595f3ea6ae0a7e2c887c2629
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-06-17 23:45:03 +0300

    21.Верстаем страницу Диалогов

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import s from './Dialogs.module.css'
 
 export const Dialogs = () => {
     return (
-        <div>
-dialog
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+                <div className={s.dialog + ' '+ s.active}>
+                    Svetlana
+                </div>
+                <div className={s.dialog}>
+                    Natasha
+                </div>
+                <div className={s.dialog}>
+                    Irina
+                </div>
+                <div className={s.dialog}>
+                    Tanya
+                </div>
+                <div className={s.dialog}>
+                    Zlata
+                </div>
+                <div className={s.dialog}>
+                    Marina
+                </div>
+
+            </div>
+        <div className={s.messages}>
+            <div className={s.message}>Hello!</div>
+            <div className={s.message}>How are you?</div>
+            <div className={s.message}>Yo!</div>
         </div>
+
+        </div>
+
     );
 };
 
