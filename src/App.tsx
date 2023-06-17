fc6475edba297325e11ddf8216075a02c0a09e93
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-06-17 19:32:58 +0300

    19. route, browser-router, маршрутизация

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,23 @@ import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Profile} from "./components/Profile/Profile";
 import {Dialogs} from './components/Dialogs/Dialogs';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 function App() {
+
   return (
+      <BrowserRouter>
     <div className={'app-wrapper'}>
        <Header/>
        <Navbar/>
         <div className={'app-wrapper-content'}>
-              <Profile/>
+            <Route path={'/dialogs'} component={Dialogs} />
+            <Route path={'/profile'} component={Profile} />
+             {/* <Profile/>*/}
            {/* <Dialogs/>*/}
         </div>
-
-
     </div>
+      </BrowserRouter>
   );
 }
 
