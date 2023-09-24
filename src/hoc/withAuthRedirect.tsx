3fc96ec77fc60b88d850304a190839264347983d
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-09-24 20:44:17 +0300

    69. High Order Component (hoc)

--- src/hoc/withAuthRedirect.tsx
+++ src/hoc/withAuthRedirect.tsx
@@ -3,18 +3,18 @@ import {Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {StateType} from '../redux/redux-store';
 
-type MapStateToPropsForRedirectType={
-    isAuth:boolean
+type MapStateToPropsForRedirectType = {
+    isAuth: boolean
 }
-let mapStateToPropsForRedirect = (state: StateType):MapStateToPropsForRedirectType => ({
+let mapStateToPropsForRedirect = (state: StateType): MapStateToPropsForRedirectType => ({
     isAuth: state.auth.isAuth
 })
 
-export function withAuthRedirect  <T>(Component: ComponentType<T>) {
+export function withAuthRedirect<T>(Component: ComponentType<T>) {
 
-    const  RedirectComponent=(props: MapStateToPropsForRedirectType)=> {
-let {isAuth,...restProps}=props
-        if (!isAuth) return <Redirect to ={'/login'} />
+    const RedirectComponent = (props: MapStateToPropsForRedirectType) => {
+        let {isAuth, ...restProps} = props
+        if (!isAuth) return <Redirect to={'/login'}/>
         return <Component {...restProps as T}/>
 
     }
