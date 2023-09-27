c785497fca3b5d8fce13e133b8b312a13f32fee5
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-09-27 08:31:22 +0300

    74.  componentDidUpdate

--- src/components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -34,6 +34,13 @@ class ProfileStatus extends React.Component<ProfileStatusType, StateType> {
             status: e.currentTarget.value
         })
     }
+componentDidUpdate(prevProps: Readonly<ProfileStatusType>, prevState: Readonly<StateType>, snapshot?: any) {
+        if (prevProps.status!==this.props.status){
+            this.setState({
+                status:this.props.status
+            })
+        }
+}
 
     render() {
         return (
